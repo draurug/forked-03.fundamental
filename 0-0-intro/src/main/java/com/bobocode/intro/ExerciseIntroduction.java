e6b21c9ec1eb8277baab6b516fd177bea740fac3
Draugur <draugur3.14@gmail.com>
2024-11-01 21:03:55 +0300

    Done

--- 0-0-intro/src/main/java/com/bobocode/intro/ExerciseIntroduction.java
+++ 0-0-intro/src/main/java/com/bobocode/intro/ExerciseIntroduction.java
@@ -1,5 +1,7 @@
 package com.bobocode.intro;
 
+import java.util.Base64;
+
 import com.bobocode.util.ExerciseNotCompletedException;
 
 /**
@@ -23,9 +25,9 @@ public class ExerciseIntroduction {
      * @return "The key to efficient learning is practice!"
      */
     public String getWelcomeMessage() {
-        // todo: implement a method and return a message according to javadoc
-        throw new ExerciseNotCompletedException(); 
-    }
+        return "The key to efficient learning is practice!";
+    }    
+    
 
     /**
      * Method encodeMessage accepts one {@link String} parameter and returns encoded {@link String}.
@@ -39,7 +41,6 @@ public class ExerciseIntroduction {
      * @return encoded message
      */
     public String encodeMessage(String message) {
-        // todo: switch to branch "completed" in order to see how it should be implemented
-        throw new ExerciseNotCompletedException();
+        return Base64.getEncoder().encodeToString(message.getBytes());
     }
 }
